60cb1b25697ca0f0e9bb8010c6c79604ff8c054f
wangmingdong <wangmingdong@navinfo.com>
2017-12-21 13:45:11 +0800

    添加vue项目

---
+++ src/components/login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="login">
+    	<mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
+    	<mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+
+		<mt-button type="primary" size="large" @click="save()">登录</mt-button>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data: function () {
+      return {
+        username: this.username,
+        password: this.password
+      };
+    },
+    methods: {
+      save () {
+        let obj = {};
+        obj.username = this.username || '';
+        obj.password = this.password || '';
+        this.$store.dispatch('login', obj);
+        // 调用action发送请求
+      }
+    }
+  };
+</script>
---
+++ src/store/getters.js
@@ -0,0 +1,21 @@
+export default {
+  user: (state) => {
+    let user = JSON.parse(localStorage.getItem('user'));
+    if (user && user.username) {
+      state = user;
+    }
+    return state;
+  },
+  list: (list) => {
+    let userList = JSON.parse(localStorage.getItem('list'));
+    console.log(userList);
+    if (userList && userList.length) {
+      list = userList;
+    }
+    return list;
+  },
+  strLength: (state) => {
+    console.log(state);
+    return state;
+  }
+};
